83b2e66869d203860e9b7f53468ec60b0467f91d
franek12345678 <109067977+franek12345678@users.noreply.github.com>
2022-07-23 11:11:52 +0200

    Początkowe pliki dla projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    ssr = 1
+    basic.showIcon(IconNames.Yes)
+})
+input.onButtonPressed(Button.B, function () {
+    ssr = 0
+    basic.showIcon(IconNames.No)
+})
+let ssr = 0
+ssr = 0
+basic.showIcon(IconNames.No)
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P0, ssr)
+})
